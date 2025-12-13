a15d2c3491bd8a3cfbe5720b4bb3de5b1120df46
Julie Evans <juliemevans17@gmail.com>
2025-12-12 23:20:36 -0600

    add theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname, '..'), // Sets the root to the parent directory
+  }
 };
 
 export default nextConfig;
